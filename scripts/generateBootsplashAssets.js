201d4cea0207c69e352491e2b7a1b0aebbac4853
Mathieu Acthernoene <zoontek@gmail.com>
2019-12-15 15:09:01 +0100

    Improve assets generation

--- scripts/generateBootsplashAssets.js
+++ scripts/generateBootsplashAssets.js
@@ -6,159 +6,252 @@ const path = require("path");
 const fs = require("fs");
 const chalk = require("chalk");
 const jimp = require("jimp");
-const meow = require("meow");
+const prompts = require("prompts");
 
 const iconName = "bootsplash_logo";
+let projectName;
 
-const logError = text => {
-  console.log("❌  " + chalk.red.bold(text));
-  process.exit(1);
-};
+const absDefaultProjectPath = path.resolve(
+  path.join(__dirname, "..", "..", ".."),
+);
+
+const defaultProjectPath = path.join(
+  ".",
+  path.relative(process.cwd(), absDefaultProjectPath),
+);
 
 const log = text => {
   console.log(chalk.dim(text));
 };
 
-const cli = meow(
-  `${chalk.bold("Usage")}
-  ${chalk.dim("$")} generate-bootsplash-assets ${chalk.green("<file>")}
+const ContentsJson = `{
+  "images": [
+    {
+      "idiom": "universal",
+      "filename": "bootsplash_logo.png",
+      "scale": "1x"
+    },
+    {
+      "idiom": "universal",
+      "filename": "bootsplash_logo@2x.png",
+      "scale": "2x"
+    },
+    {
+      "idiom": "universal",
+      "filename": "bootsplash_logo@3x.png",
+      "scale": "3x"
+    }
+  ],
+  "info": {
+    "version": 1,
+    "author": "xcode"
+  }
+}
+`;
 
-${chalk.bold("Options")}
-  --project-path  ${chalk.dim("Your React Native project path")}
-  --assets-path   ${chalk.dim("Your static assets folder path")}
+const getProjectName = projectPath => {
+  try {
+    const appJsonPath = path.join(projectPath, "app.json");
+    const appJson = fs.readFileSync(appJsonPath, "utf-8");
+    const { name } = JSON.parse(appJson);
 
-${chalk.bold("Example")}
-  ${chalk.dim(
-    "$",
-  )} generate-bootsplash-assets my_awesome_logo.png --project-path="." --assets-path="./assets"`,
+    if (!name) {
+      throw new Error("Invalid projectPath");
+    }
+
+    return name;
+  } catch (e) {
+    return false;
+  }
+};
+
+const questions = [
   {
-    pkg: {},
-    flags: {
-      projectPath: { type: "string" },
-      assetsPath: { type: "string" },
+    name: "projectPath",
+    type: "text",
+    initial: defaultProjectPath,
+    message: "The path to the root of your React Native project",
+
+    validate: value => {
+      if (!fs.existsSync(value)) {
+        return `Invalid project path. The directory ${chalk.bold(
+          value,
+        )} could not be found.`;
+      }
+
+      projectName = getProjectName(value);
+
+      if (!projectName) {
+        return `Invalid React Native project. A valid ${chalk.bold(
+          "app.json",
+        )} file could not be found.`;
+      }
+
+      return true;
     },
   },
+  {
+    name: "assetsPath",
+    type: "text",
+    initial: prev => path.join(prev, "assets"),
+    message: "The path to your static assets directory",
+
+    validate: value => {
+      if (!fs.existsSync(value)) {
+        return `Invalid assets path. The directory ${chalk.bold(
+          value,
+        )} could not be found.`;
+      }
+
+      return true;
+    },
+  },
+  {
+    name: "iconPath",
+    type: "text",
+    message: "Your original icon file",
+    initial: prev => path.join(prev, "bootsplash_logo_original.png"),
+
+    validate: value => {
+      if (!fs.existsSync(value)) {
+        return `Invalid icon file path. The file ${chalk.bold(
+          value,
+        )} could not be found.`;
+      }
+
+      return true;
+    },
+  },
+  {
+    name: "iconWidth",
+    type: "number",
+    message:
+      "The desired icon width (in dp - we recommand approximately ~100)?",
+    initial: 100,
+    min: 1,
+    max: 1000,
+  },
+  {
+    name: "confirmation",
+    type: "confirm",
+    message:
+      "Are you sure? All the existing bootsplash images will be overwritten!",
+    initial: true,
+  },
+];
+
+prompts(questions).then(
+  ({ projectPath, assetsPath, iconPath, iconWidth: w1, confirmation }) => {
+    if (!projectPath || !assetsPath || !iconPath || !w1 || !confirmation) {
+      process.exit(1);
+    }
+
+    const imageMap = [];
+
+    jimp
+      .read(iconPath)
+      .then(image => {
+        const h = size =>
+          Math.ceil(size * (image.bitmap.height / image.bitmap.width));
+
+        const w15 = w1 * 1.5;
+        const w2 = w1 * 2;
+        const w3 = w1 * 3;
+        const w4 = w1 * 4;
+
+        const androidResPath = path.join(
+          projectPath,
+          "android",
+          "app",
+          "src",
+          "main",
+          "res",
+        );
+
+        if (fs.existsSync(androidResPath)) {
+          const fileName = iconName + ".png";
+
+          imageMap.push(
+            [path.join(androidResPath, "mipmap-mdpi", fileName), [w1, h(w1)]],
+            [path.join(androidResPath, "mipmap-hdpi", fileName), [w15, h(w15)]],
+            [path.join(androidResPath, "mipmap-xhdpi", fileName), [w2, h(w2)]],
+            [path.join(androidResPath, "mipmap-xxhdpi", fileName), [w3, h(w3)]],
+            [
+              path.join(androidResPath, "mipmap-xxxhdpi", fileName),
+              [w4, h(w4)],
+            ],
+          );
+        } else {
+          log(
+            `No ${androidResPath} directory found. Skipping android generation…`,
+          );
+        }
+
+        const iosImagesPath = path.join(
+          projectPath,
+          "ios",
+          projectName,
+          "Images.xcassets",
+        );
+
+        if (fs.existsSync(iosImagesPath)) {
+          const iosImageSetPath = path.join(
+            iosImagesPath,
+            "BootSplashLogo.imageset",
+          );
+
+          if (!fs.existsSync(iosImageSetPath)) {
+            fs.mkdirSync(iosImageSetPath);
+          }
+
+          fs.writeFileSync(
+            path.join(iosImageSetPath, "Contents.json"),
+            ContentsJson,
+            "utf-8",
+          );
+
+          imageMap.push(
+            [path.join(iosImageSetPath, iconName + ".png"), [w1, h(w1)]],
+            [path.join(iosImageSetPath, iconName + "@2x.png"), [w2, h(w2)]],
+            [path.join(iosImageSetPath, iconName + "@3x.png"), [w3, h(w3)]],
+          );
+        } else {
+          log(`No ${iosImagesPath} directory found. Skipping iOS generation…`);
+        }
+
+        imageMap.push(
+          [path.join(assetsPath, iconName + ".png"), [w1, h(w1)]],
+          [path.join(assetsPath, iconName + "@1,5x.png"), [w15, h(w15)]],
+          [path.join(assetsPath, iconName + "@2x.png"), [w2, h(w2)]],
+          [path.join(assetsPath, iconName + "@3x.png"), [w3, h(w3)]],
+          [path.join(assetsPath, iconName + "@4x.png"), [w4, h(w4)]],
+        );
+
+        console.log("👍  Looking good! Generating images…");
+
+        return Promise.all(
+          imageMap.map(([path, [width, height]]) =>
+            image
+              .clone()
+              .cover(width, height)
+              .write(path),
+          ),
+        );
+      })
+      .then(() => {
+        imageMap.map(([path, [width, height]]) => {
+          log(`✨ ${path} (${width}x${height})`);
+        });
+      })
+      .then(() =>
+        console.log(
+          `✅  Done! Thanks for using ${chalk.underline(
+            "react-native-bootsplash",
+          )}.`,
+        ),
+      )
+      .catch(error => {
+        console.log(chalk.red.bold(error.toString()));
+      });
+  },
 );
-
-const defaultProjectPath = path.resolve(path.join(__dirname, "..", "..", ".."));
-
-let { projectPath = defaultProjectPath } = cli.flags;
-let [iconPath] = cli.input;
-let { assetsPath = path.join(projectPath, "assets") } = cli.flags;
-
-if (!iconPath) {
-  logError(
-    "No icon file path provided!\r\nType `generate-bootsplash-assets --help` for usage informations.",
-  );
-}
-
-if (!fs.existsSync(iconPath)) {
-  logError(`Invalid icon path. The file ${iconPath} could not be found.`);
-}
-if (!fs.existsSync(projectPath)) {
-  logError(
-    `Invalid project path. The directory ${projectPath} could not be found.`,
-  );
-}
-
-projectPath = path.resolve(projectPath);
-iconPath = path.resolve(iconPath);
-assetsPath = path.resolve(assetsPath);
-
-const appJsonPath = path.join(projectPath, "app.json");
-
-if (!fs.existsSync(appJsonPath)) {
-  logError(
-    `Invalid React Native project. The expected ${appJsonPath} file could not be found.`,
-  );
-}
-
-let projectName;
-
-try {
-  projectName = JSON.parse(fs.readFileSync(appJsonPath, "utf-8")).name;
-  console.log(projectName);
-
-  if (!projectName) {
-    throw new Error("Invalid project name");
-  }
-} catch (e) {
-  logError(
-    `Invalid React Native project. ${appJsonPath} does not define a valid project name.`,
-  );
-}
-
-const imageMap = [];
-
-if (fs.existsSync(assetsPath)) {
-  imageMap.push(
-    [path.join(assetsPath, iconName + ".png"), [100, 100]],
-    [path.join(assetsPath, iconName + "@1,5x.png"), [150, 150]],
-    [path.join(assetsPath, iconName + "@2x.png"), [200, 200]],
-    [path.join(assetsPath, iconName + "@3x.png"), [300, 300]],
-    [path.join(assetsPath, iconName + "@4x.png"), [400, 400]],
-  );
-} else {
-  log(`No ${assetsPath} directory found. Skipping static assets generation…`);
-}
-
-const androidResPath = path.join(projectPath, "android", "src", "main", "res");
-
-if (fs.existsSync(androidResPath)) {
-  const fileName = iconName + ".png";
-
-  imageMap.push(
-    [path.join(androidResPath, "mipmap-mdpi", fileName), [100, 100]],
-    [path.join(androidResPath, "mipmap-hdpi", fileName), [150, 150]],
-    [path.join(androidResPath, "mipmap-xhdpi", fileName), [200, 200]],
-    [path.join(androidResPath, "mipmap-xxhdpi", fileName), [300, 300]],
-    [path.join(androidResPath, "mipmap-xxxhdpi", fileName), [400, 400]],
-  );
-} else {
-  log(`No ${androidResPath} directory found. Skipping android generation…`);
-}
-
-const iosImagesPath = path.join(
-  projectPath,
-  "ios",
-  projectName,
-  "Images.xcassets",
-);
-
-if (fs.existsSync(iosImagesPath)) {
-  const iosImageSetPath = path.join(iosImagesPath, "BootSplashLogo.imageset");
-
-  if (!fs.existsSync(iosImageSetPath)) {
-    fs.mkdirSync(iosImageSetPath);
-  }
-
-  imageMap.push(
-    [path.join(iosImageSetPath, iconName + ".png"), [100, 100]],
-    [path.join(iosImageSetPath, iconName + "@2x.png"), [200, 200]],
-    [path.join(iosImageSetPath, iconName + "@3x.png"), [300, 300]],
-  );
-} else {
-  log(`No ${iosImagesPath} directory found. Skipping iOS generation…`);
-}
-
-console.log("👍  Looks good! Preparing images…");
-
-jimp
-  .read(iconPath)
-  .then(image =>
-    Promise.all(
-      imageMap.map(([path, [width, height]]) =>
-        image
-          .clone()
-          .cover(width, height)
-          .write(path),
-      ),
-    ),
-  )
-  .then(() => {
-    imageMap.map(([path, [width, height]]) => {
-      log(`✨ ${path} (${width} x ${height})`);
-    });
-  })
-  .then(() => console.log("✅  Done!"))
-  .catch(e => logError(e.toString()));
