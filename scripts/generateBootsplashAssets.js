672edca8ce5232dd23b4ddc530e9aee297d9424b
Mathieu Acthernoene <zoontek@gmail.com>
2019-12-15 15:56:32 +0100

    Remove interrogation mark

--- scripts/generateBootsplashAssets.js
+++ scripts/generateBootsplashAssets.js
@@ -125,8 +125,7 @@ const questions = [
   {
     name: "iconWidth",
     type: "number",
-    message:
-      "The desired icon width (in dp - we recommand approximately ~100)?",
+    message: "The desired icon width (in dp - we recommand approximately ~100)",
     initial: 100,
     min: 1,
     max: 1000,
