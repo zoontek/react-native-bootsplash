9d4faf00a355d43c6243d738d07ed1bbd393792c
Ray Deck <ray@raydeck.com>
2020-04-05 21:55:29 -0500

    Extracting show and hide

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { NativeModules } from "react-native";
 const { RNBootSplash } = NativeModules;
-
+const hide = (config = {}) => {
+  RNBootSplash.hide({ duration: 0, ...config }.duration);
+};
+const show = (config = {}) => {
+  RNBootSplash.show({ duration: 0, ...config }.duration);
+};
 export default {
-  hide(config = {}) {
-    RNBootSplash.hide({ duration: 0, ...config }.duration);
-  },
-  show(config = {}) {
-    RNBootSplash.show({ duration: 0, ...config }.duration);
-  },
+  show,
+  hide,
 };
 export { show, hide };
