9e5317b51c5b088ada88cd81c246cbcfd9699e73
Mathieu Acthernoene <zoontek@gmail.com>
2020-08-13 18:29:25 +0200

    Update example

--- example/android/app/src/main/java/com/rnbootsplashexample/MainActivity.java
+++ example/android/app/src/main/java/com/rnbootsplashexample/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this, R.style.AppTheme);
     super.onCreate(savedInstanceState);
-    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
   }
 }
