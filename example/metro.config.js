eaa0365b0bec8bf28763fcead78bfc7e2a73c077
Mathieu Acthernoene <zoontek@gmail.com>
2021-03-16 10:47:56 +0100

    Bump dependencies / update to RN 0.64

--- example/metro.config.js
+++ example/metro.config.js
@@ -3,7 +3,7 @@ module.exports = {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
