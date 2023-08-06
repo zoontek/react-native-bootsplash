5f4bbeb11c20752be541952a2c3b678b2c730aa8
Mathieu Acthernoene <zoontek@gmail.com>
2023-08-07 01:49:27 +0200

    Unify function declarations

--- src/NativeRNBootSplash.ts
+++ src/NativeRNBootSplash.ts
@@ -2,7 +2,7 @@ import type { TurboModule } from "react-native";
 import { TurboModuleRegistry } from "react-native";
 
 export interface Spec extends TurboModule {
-  getConstants: () => { navigationBarHeight: number; statusBarHeight: number };
+  getConstants(): { navigationBarHeight: number; statusBarHeight: number };
   hide(fade: boolean): Promise<void>;
   isVisible(): Promise<boolean>;
 }
