916374be7c85912ac85245860af89956e91e9f0e
Mathieu Acthernoene <zoontek@gmail.com>
2023-08-12 15:16:43 +0200

    Add webpack to the example app

--- example/index.js
+++ example/index.js
@@ -3,3 +3,8 @@ import { name as appName } from "./app.json";
 import { App } from "./src/App";
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (typeof document !== "undefined") {
+  const rootTag = document.getElementById("root");
+  AppRegistry.runApplication(appName, { rootTag });
+}
---
+++ example/webpack.config.js
@@ -0,0 +1,45 @@
+// https://necolas.github.io/react-native-web/docs/multi-platform
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const fromRoot = (_) => path.resolve(__dirname, _);
+
+module.exports = {
+  mode: "development",
+  entry: fromRoot("index.js"),
+  output: {
+    path: fromRoot("dist"),
+    filename: "bundle.web.js",
+  },
+  devServer: {
+    static: { directory: fromRoot("dist") },
+    devMiddleware: { publicPath: "/" },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: fromRoot("index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: { loader: "babel-loader" },
+        include: [fromRoot("index.js"), fromRoot("src")],
+      },
+      {
+        test: /\.(jpe?g|png|svg)$/,
+        use: {
+          loader: "url-loader",
+          options: { name: "[name].[ext]", esModule: false },
+        },
+      },
+    ],
+  },
+  resolve: {
+    alias: { "react-native$": "react-native-web" },
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+      .map((extension) => [".web" + extension, extension])
+      .flat(),
+  },
+};
