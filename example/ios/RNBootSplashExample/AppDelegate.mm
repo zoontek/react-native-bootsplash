595f2913db46538456d500808fea6543342b39f2
Mathieu Acthernoene <zoontek@gmail.com>
2023-12-12 01:53:45 +0100

    Update to react-native 0.73

--- example/ios/RNBootSplashExample/AppDelegate.mm
+++ example/ios/RNBootSplashExample/AppDelegate.mm
@@ -11,10 +11,16 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self getBundleURL];
+}
+
+- (NSURL *)getBundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
