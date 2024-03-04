5aea6035e9c7f9540962f3dfdd641a44171c1e7a
Gurupad Mamadapur <gurupadmamadapur@gmail.com>
2024-03-04 20:32:17 +0530

    fixes 552; de-initialize RNBootSplashModule onHostDestroy

--- android/src/oldarch/com/zoontek/rnbootsplash/RNBootSplashModule.java
+++ android/src/oldarch/com/zoontek/rnbootsplash/RNBootSplashModule.java
@@ -3,6 +3,7 @@ package com.zoontek.rnbootsplash;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -12,10 +13,11 @@ import com.facebook.react.module.annotations.ReactModule;
 import java.util.Map;
 
 @ReactModule(name = RNBootSplashModuleImpl.NAME)
-public class RNBootSplashModule extends ReactContextBaseJavaModule {
+public class RNBootSplashModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
   public RNBootSplashModule(ReactApplicationContext reactContext) {
     super(reactContext);
+    reactContext.addLifecycleEventListener(this);
   }
 
   @NonNull
@@ -38,5 +40,18 @@ public class RNBootSplashModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void isVisible(Promise promise) {
     RNBootSplashModuleImpl.isVisible(promise);
+  }
+
+  @Override
+  public void onHostResume() {
+  }
+
+  @Override
+  public void onHostPause() {
+  }
+
+  @Override
+  public void onHostDestroy() {
+    RNBootSplashModuleImpl.deInit();
   }
 }
