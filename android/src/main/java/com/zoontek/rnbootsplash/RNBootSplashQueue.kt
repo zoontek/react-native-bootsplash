6554ed3a35f19ba84bc518279516f77d58098903
Mathieu Acthernoene <zoontek@gmail.com>
2024-10-13 13:11:55 +0200

    Migrate RNBootSplashQueue

---
+++ android/src/main/java/com/zoontek/rnbootsplash/RNBootSplashQueue.kt
@@ -0,0 +1,26 @@
+package com.zoontek.rnbootsplash
+
+import java.util.Vector
+
+/**
+ * Represents a first-in-first-out (FIFO) thread safe queue of objects.
+ * Its source code is based on Java internal `Stack`.
+ */
+class RNBootSplashQueue<E> : Vector<E>() {
+
+  @Synchronized
+  fun shift(): E? {
+    if (size == 0) {
+      return null
+    }
+
+    val item = elementAt(0)
+    removeElementAt(0)
+
+    return item
+  }
+
+  fun push(item: E) {
+    addElement(item)
+  }
+}
