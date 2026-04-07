1dfdabc67c5a6d9b7f48b3a72aff81a68685d1d8
Mathieu Acthernoene <zoontek@gmail.com>
2026-04-07 12:52:18 +0200

    Use ArrayDeque instead of Vector

--- android/src/main/java/com/zoontek/rnbootsplash/RNBootSplashQueue.kt
+++ android/src/main/java/com/zoontek/rnbootsplash/RNBootSplashQueue.kt
@@ -1,26 +1,13 @@
 package com.zoontek.rnbootsplash
 
-import java.util.Vector
+/** Represents a first-in-first-out (FIFO) thread safe queue of objects. */
+class RNBootSplashQueue<E> {
+  private val queue = ArrayDeque<E>()
 
-/**
- * Represents a first-in-first-out (FIFO) thread safe queue of objects.
- * Its source code is based on Java internal `Stack`.
- */
-class RNBootSplashQueue<E> : Vector<E>() {
+  @Synchronized fun shift(): E? = queue.removeFirstOrNull()
 
   @Synchronized
-  fun shift(): E? {
-    if (isEmpty()) {
-      return null
-    }
-
-    val item = elementAt(0)
-    removeElementAt(0)
-
-    return item
-  }
-
   fun push(item: E) {
-    addElement(item)
+    queue.addLast(item)
   }
 }
